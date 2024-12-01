2cc07eabee5f4f9845680403a291f267756b826e
lr1descent <H2802710247@163.com>
2024-12-01 23:09:50 +0800

    测试远程commit的撤销

--- src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
+++ src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+// test un commit
+// 测试远程commit的撤销
 @SpringBootApplication
 public class SpringbootMybatisApplication {
     public static void main(String[] args) {
