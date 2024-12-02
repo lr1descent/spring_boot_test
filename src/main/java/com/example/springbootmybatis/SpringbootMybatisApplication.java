f66e3e6e619eac94c408f47713acb0795f469641
lr1descent <H2802710247@163.com>
2024-12-02 11:55:26 +0800

    Revert "测试远程commit的撤销"
    This reverts commit 2cc07eabee5f4f9845680403a291f267756b826e.

--- src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
+++ src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-// test un commit
-// 测试远程commit的撤销
 @SpringBootApplication
 public class SpringbootMybatisApplication {
     public static void main(String[] args) {
